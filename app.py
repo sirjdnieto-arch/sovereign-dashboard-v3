69625190e9cf722db7ea1860a3fe419944607b8c
sirjdnieto-arch <sirjdnieto@gmail.com>
2026-06-03 13:37:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ ALL_TICKERS = [
     "PG","XOM","UNH","JPM","HD","LLY","MA","CVX","ABBV","KO","PEP",
     "COST","BAC","CRM","NFLX","ABT","MCD","LMT","EL","NEE","CAT","MRK",
     "TPL","ASML","ADBE","AVGO","CSCO","CMCSA","AMD","TXN","QCOM","AMAT","LITE","LRCX","COHR","NEE",
-    "NEM", "ULTA", "IT","FOXA","LUV","VLO", "ADP", "FN", "POET", 
+    "NEM", "ULTA", "IT","FOXA","LUV","VLO", "ADP", "FN", "POET", "KEYS", "HPE", "MRVL", "AMAT",
     "INTU","VRTX","ZS","PLTR","CSU.TO","MU","LVMUY","SAP","OR.PA","TTE","SATS","ON",
     "MC.PA","SIE.DE","ENGI.PA","AIR.PA","ALV.DE","EL.PA","AI.PA","BNP.PA",
     "SAN.PA","KER.PA","SU.PA","NESN.SW","LIN.DE","VOW3.DE","BMW.DE","ADS.DE",
@@ -189,7 +189,7 @@ ALL_TICKERS = [
     "TEF.MC","IBE.MC","REP.MC","FER.MC","ACX.MC","ACS.MC","AENA.MC","ANA.MC",
     "IAG.MC","LOG.MC","MAP.MC","PUIG.MC","NTGY.MC","ELE.MC","IDR.MC","PDD",
     "NIO","TCEHY","BZUN","FUTU","MOMO","MNSO","TAL","EDU","WB","XPEV",
-    "GC=F","SI=F","BTC-USD","ETH-USD","XRP-USD","CRCL"
+    "GC=F","SI=F","BTC-USD","ETH-USD","XRP-USD", "ETH-USD", "CRCL"
 ]
 
 GRUPOS = {
